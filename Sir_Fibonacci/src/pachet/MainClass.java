913410597da53f5d4f1ba7f3b84b0e3df092ba34
Rut Catana <rut.catana@gmail.com>
2021-05-14 00:18:22 +0300

    Citire numar de la tastatura

--- Sir_Fibonacci/src/pachet/MainClass.java
+++ Sir_Fibonacci/src/pachet/MainClass.java
@@ -1,11 +1,20 @@
 package pachet;
 
+import java.util.Scanner;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello");
+		System.out.println("Program care genereaza sirul lui Fibonacci");
+		
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner( System.in );
 
+		System.out.print( "Introduceti numarul dorit: " );
+		int nr = scanner.nextInt();
+		
+		System.out.println("Ati introdus: "+nr);
 	}
 
 }
